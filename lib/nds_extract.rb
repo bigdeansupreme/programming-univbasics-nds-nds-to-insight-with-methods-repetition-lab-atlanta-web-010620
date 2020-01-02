ab02b7ab0948a1aafad12400cb65880737374afa
Dino Kunovac <dinokunovac@gmail.com>
2020-01-02 15:40:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,15 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  
+  names = []
+  i = 0
+  
+  while i < source.length do
+    names << source[i][:name]
+    i += 1
+  end
+  names
 end
 
 def total_gross(source)
@@ -38,6 +46,8 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  
 end
 
 
